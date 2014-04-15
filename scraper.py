1b05e498b0541e524734745db49ffb82ebcadb2f
gcampedelli <gcampedelli@users.noreply.github.com>
2014-04-15 18:31:02 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,7 +8,7 @@ from datetime import date, timedelta
 def scrapeTable(root, numId):
     
     #annonces
-    for annonce in root.cssselect("div#primaryResults"):
+    for annonce in root.cssselect("table"):
 
         #enregistrement des resultats
         record = {}      
@@ -16,83 +16,30 @@ def scrapeTable(root, numId):
         for uneAnnonce in annonce.cssselect("table tr.odd"):
 
             #job url
-            for jobUrl in uneAnnonce.cssselect("div.jobTitleContainer a"):
+            for jobUrl in uneAnnonce.cssselect("a"):
                 record["url"]=jobUrl.get('href')
                 
-            print 'Job url : ', record["url"]
+            print 'url : ', record["url"]
 
-            #company name
-            for nomCompany in uneAnnonce.cssselect("div.companyContainer a"):
-                record["company"]=nomCompany.text
-                
-            #annonceName
-            for annonceName in uneAnnonce.cssselect("div.jobTitleContainer a"):
-                record["annonceName"]=annonceName.text
+            #email
+            for email in uneAnnonce.cssselect("td"):
+                record["email"]=email.text
         
-            #salaire
-            for salaire in uneAnnonce.cssselect("div.companyContainer div.fnt13"):
-                record["salaire"]=salaire.text
-        
-            #date
-            for parution in uneAnnonce.cssselect("div.jobTitleCol div.fnt20"):
-                if "Aujourd" in parution.text_content().strip().split("'")[0]:
-                    record["parution"]=time.strftime('%d/%m/%y', time.localtime())
-                else:
-                    s = re.findall('\d+', parution.text_content().strip())
-                    numDays = int(s[0])
-                    print "numDays", numDays
-                    d = date.today()-timedelta(days=numDays)
-                    record["parution"]=d.strftime('%d/%m/%y')
-
-            print "parution : ", record["parution"]
-    
-            #region
-            for region in uneAnnonce.cssselect("div.jobLocationSingleLine a"):
-                record["region"]=region.text
-    
-            #id
-            record["id"]=numId
-
             #save in datastore
             scraperwiki.sqlite.save(["id"], record)
             numId=numId+1
 
         for uneAnnonce in annonce.cssselect("table tr.even"):
             #job url
-            for jobUrl in uneAnnonce.cssselect("div.jobTitleContainer a"):
+            for jobUrl in uneAnnonce.cssselect("a"):
                 record["url"]=jobUrl.get('href')
                 
-            print 'Job url : ', record["url"]
-
-            #company name
-            for nomCompany in uneAnnonce.cssselect("div.companyContainer a"):
-                record["company"]=nomCompany.text
-                
-            #annonceName
-            for annonceName in uneAnnonce.cssselect("div.jobTitleContainer a"):
-                record["annonceName"]=annonceName.text
+            print 'url : ', record["url"]
         
             #salaire
-            for salaire in uneAnnonce.cssselect("div.companyContainer div.fnt13"):
-                record["salaire"]=salaire.text
+            for email in uneAnnonce.cssselect("div.companyContainer div.fnt13"):
+                record["email"]=email.text
         
-            #date
-            for parution in uneAnnonce.cssselect("div.jobTitleCol div.fnt20"):
-                if "Aujourd" in parution.text_content().strip().split("'")[0]:
-                    record["parution"]=time.strftime('%d/%m/%y', time.localtime())
-                else:
-                    s = re.findall('\d+', parution.text_content().strip())
-                    numDays = int(s[0])
-                    print "numDays", numDays
-                    d = date.today()-timedelta(days=numDays)
-                    record["parution"]=d.strftime('%d/%m/%y')
-
-            print "parution : ", record["parution"]
-    
-            #region
-            for region in uneAnnonce.cssselect("div.jobLocationSingleLine a"):
-                record["region"]=region.text
-    
             #id
             record["id"]=numId
             
@@ -105,11 +52,11 @@ def scrapeTable(root, numId):
 
 
 #start
-sUrlBase="http://offres.monster.fr/offres-d-emploi/?pg="
+sUrlBase="https://www.secom.planalto.gov.br/consea/boletins.nsf/01ContatoxNome?OpenView&Start="
 numId=0
 
-for page in range(40):
-    page=page+1
+for page in range(50):
+    page=page+30
     sUrl=sUrlBase + str(page) + "&cy=LU"
     print sUrl
 
